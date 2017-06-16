14e09edbd4ff5b4a39a026ba3714d2669f15d690
John Murphy <murphyjohn@users.noreply.github.com>
2017-06-16 09:11:42 +0100

    Add files via upload

---
+++ Polymorphism1/Polygon.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism1
+{
+    class Polygon
+    {
+        public virtual void Draw()
+        {
+            Console.WriteLine("Drawing: Polygon");
+        }
+    }
+
+    class Rectangle : Polygon
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing: Rectangle");
+        }
+    }
+
+    class Triangle : Polygon
+    {
+        public new void Draw()
+        {
+            Console.WriteLine("Drawing: Triangle");
+        }
+
+        public void TriangleMethod()
+        {
+            Console.WriteLine("Triangle Method");
+        }
+    }
+}
---
+++ Polymorphism1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<Polygon> polygons = new List<Polygon>();
+            //polygons.Add(new Polygon());
+            //polygons.Add(new Rectangle());
+            //polygons.Add(new Triangle());
+
+            //foreach (Polygon p in polygons)
+            //{
+            //    p.Draw();
+            //}
+
+            Triangle t = new Triangle();
+            t.Draw();
+            Rectangle r = new Rectangle();
+            r.Draw();
+
+            Console.WriteLine("\n\n\n");
+            Polygon p = t;
+            p.Draw();
+            Polygon p2 = r;
+            p2.Draw();
+
+            //Testing that Methods must be declared in the class of the reference type first. like in Java
+            //Polygon testTriangle = new Triangle();
+            //testTriangle.TriangleMethod();
+        }
+    }
+}
